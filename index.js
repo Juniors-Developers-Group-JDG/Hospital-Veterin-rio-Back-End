5e2a201c019e0d25e82c26e08d0fd6beff25c78e
luix gabriel <luisgasbriel@gmail.com>
2023-05-18 08:32:10 -0300

    FIrst Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(8000, () => {
+  console.log('servidor ON');
+});
